f7c881d1f8471d077484f11a3cd5e22488bf01f2
javajigi <javajigi@gmail.com>
2019-01-19 08:38:04 +0900

    add tdd, refactoring test code

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import org.junit.Test;
+import support.test.BaseTest;
+
+public class StringCalculatorTest extends BaseTest {
+    @Test
+    public void 덧셈() {
+
+    }
+
+    @Test
+    public void 뺄셈() {
+
+    }
+
+    @Test
+    public void 곱셈() {
+
+    }
+
+    @Test
+    public void 나눗셈() {
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 값_null() {
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 값_빈공백() {
+
+    }
+
+    @Test
+    public void calculate() {
+
+    }
+}
