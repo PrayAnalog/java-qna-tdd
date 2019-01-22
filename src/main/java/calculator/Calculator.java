e68807a45b9880e0416f986307a5c833f14d3ee8
Gwan0 <prayanalog@gmail.com>
2019-01-22 15:31:24 +0900

    Add isValid function for check validity using regex
    Dependency : junit5 version 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,13 +1,50 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
-    public int add(int a, int b) {
-        return a + b;
+    public int add(String text) {
+        if (!isValid(text)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (isBlank(text)) {
+            return 0;
+        }
+
+
+        Matcher m = Pattern.compile("//(.*)\n(.*)").matcher(text);
+        if (m.find()) {
+            String delimiter = m.group(1);
+            return addWithGivenDelimiter(m.group(2), delimiter);
+        }
+        return addWithGivenDelimiter(text, "[,:]");
+
     }
 
-    public int sub(int a, int b) {
-        return a - b;
+    private boolean isValid(String text) {
+        Matcher m = Pattern.compile("()|//(.+)\n(.*)|(([0-9]+)([,:]([0-9]+))*)").matcher(text);
+        return m.find();
     }
 
+    public int addWithGivenDelimiter(String text, String delimiter) {
+        if (delimiter.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] values = text.split(delimiter);
+        int result = Integer.parseInt(values[0]);
+
+        for (int i = 1; i < values.length; i++) {
+            result = result + Integer.parseInt(values[i]);
+        }
+
+        return result;
+    }
+
+    public boolean isBlank(String text) {
+        return text.equals("");
+    }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,38 +1,92 @@
 package calculator;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class CalculatorTest {
     private Calculator cal;
 
-    @Before
-    public void setup() {
+    @BeforeEach
+
+    public void 시작() {
         cal = new Calculator();
     }
 
     @Test
-    public void 덧셈_1() {
+    public void 빈칸일때_결과는_0() {
         assertThat(cal.add("")).isEqualTo(0);
     }
 
+
     @Test
-    public void 덧셈_2() {
-        assertThat(cal.add("1,2")).isEqualTo(0);
+    public void null일때_예외던짐() {
+        assertThrows(RuntimeException.class, () -> {
+            cal.add(null);
+        });
     }
 
     @Test
-    public void 덧셈_3() {
-        assertThat(cal.add("1,2,3")).isEqualTo(0);
+    public void 입력이_1개일_경우() {
+        assertThat(cal.add("1")).isEqualTo(1);
     }
 
     @Test
-    public void 덧셈_4() {
-        assertThat(cal.add("1,2:3")).isEqualTo(0);
+    public void 입력이_2개일_경우() {
+        assertThat(cal.add("1,2")).isEqualTo(3);
+    }
+
+    @Test
+    public void 입력이_3개일_경우() {
+        assertThat(cal.add("1,2,3")).isEqualTo(6);
+    }
+
+    @Test
+    public void 구분자가_다른_경우() {
+        assertThat(cal.add("1,2:3")).isEqualTo(6);
+    }
+
+    @Test
+    public void 음수도_입력된_경우() {
+        assertThat(cal.add("-1,2:3")).isEqualTo(4);
+    }
+
+    @Nested
+    public class 숫자_이외의_값이_입력된_경우 {
+
+
+        @Test
+        void _앞에() {
+            assertThrows(RuntimeException.class, () -> {
+                cal.add("null,2:3");
+            });
+
+        }
+
+        @Test
+        void _뒤에() {
+            assertThrows(RuntimeException.class, () -> {
+                cal.add("2:null");
+            });
+        }
+
+    }
+
+    @Test
+    void 구분자_지정() {
+        assertThat(cal.add("//;\n1;2;3")).isEqualTo(6);
+    }
+
+    @Test
+    void 구분자_없는데_지정문자_있음() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            cal.add("//\n1;2;3");
+        });
     }
 
 }
